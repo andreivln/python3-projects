c46979ac82484b8f574812b560d8780adc6d8232
Valeanu Andrei <118607615+andreivln@users.noreply.github.com>
2023-08-15 14:44:58 +0300

    Create data_manager.py

---
+++ flight-deals-alert/data_manager.py
@@ -0,0 +1,46 @@
+import requests
+from pprint import pprint
+
+SHEET_API_ENDPOINT = "https://api.sheety.co/be82924dcec55363d7118714b006609a/flightDealsAndrei/prices"
+BEARER = "dfsfsd324sdfsfdfsdf34essdf"
+
+headers = {
+    "Authorization": f"Bearer {BEARER}",
+    "Content-Type": "application/json"
+}
+
+
+
+class DataManager:
+  
+
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+      
+        response = requests.get(url=SHEET_API_ENDPOINT, headers=headers)
+        data = response.json()
+        self.destination_data = data["prices"]
+
+   
+        # pprint(data)
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEET_API_ENDPOINT}/{city['id']}",
+                headers=headers,
+                json=new_data
+            )
+            print(response.text)
+
+
+
